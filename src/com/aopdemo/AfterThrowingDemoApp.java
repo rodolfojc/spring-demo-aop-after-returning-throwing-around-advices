92d1095e89b89d0c7bc6d28b434af1e450b58d61
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-03-09 21:37:34 +0000

    @AfterThrowing - Preparing
    Demo

---
+++ src/com/aopdemo/AfterThrowingDemoApp.java
@@ -0,0 +1,40 @@
+package com.aopdemo;
+
+import java.util.List;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.aopdemo.dao.AccountDAO;
+import com.aopdemo.dao.MembershipDAO;
+
+public class AfterThrowingDemoApp {
+
+	public static void main(String[] args) {
+		
+		//READ CONFIG JAVA CLASS
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		//GET THE BEAN FROM SPRING CONTAINER
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		//CALL METHOD FINDACCOUNT
+		List<Account> theAccounts = null;
+		
+		try{
+			theAccountDAO.findAccounts();
+		}catch (Exception exc) {
+			System.out.println("\nMain program... Caught exception: "+exc);
+		}
+		
+		//DISPLAY ACCOUNTS
+		System.out.println("\n\nMain program: AfterReturningDemoApp");
+		System.out.println("-----");
+		
+		System.out.println(theAccounts);
+		
+		//CLOSE CONTEXT
+		context.close();
+		
+	}
+
+}
